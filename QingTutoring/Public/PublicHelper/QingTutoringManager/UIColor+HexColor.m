b6cb7219bb5c1ae275b94ba74da21619d2a94fe7
sunzongtang <sun.zongtang@immomo.com>
2019-01-22 16:01:38 +0800

    ignore file

--- QingTutoring/Public/PublicHelper/QingTutoringManager/UIColor+HexColor.m
+++ QingTutoring/Public/PublicHelper/QingTutoringManager/UIColor+HexColor.m
@@ -46,4 +46,44 @@
     return [UIColor colorWithRed:((float)red/ 255.0f) green:((float)green/ 255.0f) blue:((float)blue/ 255.0f) alpha:alpha];
 }
 
++ (UIColor *)colorWithARGBHexString:(NSString *)hex {
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    if ([cString length] < 6)
+        return nil;
+    if ([cString hasPrefix:@"#"])
+        cString = [cString substringFromIndex:1];
+    if ([cString length] != 6 && [cString length] != 8)
+        return nil;
+    
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    
+    NSString *aString = @"FF";
+    if (cString.length == 8) { //
+        aString = [cString substringWithRange:range];
+        range.location += 2;
+    }
+    
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location += 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location += 2;
+    NSString *bString = [cString substringWithRange:range];
+    
+    unsigned int a, r, g, b;
+    [[NSScanner scannerWithString:aString] scanHexInt:&a];
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:((float) a / 255.0f)];
+}
+
 @end
